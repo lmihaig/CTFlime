0eebd2b3f17c1549bc9bac5371fb46abd5680cd4
lmihaig <licu.mihai21@gmail.com>
2022-01-16 17:09:25 +0200

    moved to one single masterfile

--- app/__init__.py
+++ app/__init__.py
@@ -3,16 +3,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 
-# Create a file (in the main directory) named secrets.txt that has only your password on one line
-path = Path(__file__).parent.parent
-print(path)
-with open(f"{path}\secrets.txt", "r", encoding="utf-8") as secretfile:
+with open(f"secrets.txt", "r", encoding="utf-8") as secretfile:
     password = secretfile.readline()
 
 app = Flask(__name__, instance_relative_config=True)
 app.config["SQLALCHEMY_DATABASE_URI"] = f"mysql://root:{password}@localhost/ctflime"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # dumb line to surpress warnings
 db = SQLAlchemy(app)
+app.secret_key = f"{password}"
 
 # needs these comments to stop pylint from complaining and autopep8 from moving the damn line
 # pylint: disable=wrong-import-position
--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from msilib import Table
-from flask import render_template, request
+from flask import render_template, request, redirect, url_for, session, flash
 from sqlalchemy.ext.automap import automap_base
 from app import app, db
 
@@ -8,105 +8,145 @@ from app import app, db
 # pylint: disable=no-member
 Base = automap_base()
 Base.prepare(db.engine, reflect=True)
-Tables = {"tari": Base.classes.tari,
-          "echipe": Base.classes.echipe,
-          "utilizatori": Base.classes.utilizatori,
-          "autori": Base.classes.autori,
-          "concursuri_ctf": Base.classes.concursuri_ctf,
-          "probleme": Base.classes.probleme,
-          "rezolvari": Base.classes.rezolvari,
-          "servere": Base.classes.servere,
-          "incercari": Base.classes.incercari
+Tables = {'tari': Base.classes.tari,
+          'echipe': Base.classes.echipe,
+          'utilizatori': Base.classes.utilizatori,
+          'autori': Base.classes.autori,
+          'concursuri_ctf': Base.classes.concursuri_ctf,
+          'probleme': Base.classes.probleme,
+          'rezolvari': Base.classes.rezolvari,
+          'servere': Base.classes.servere,
+          'incercari': Base.classes.incercari
           }
 
 
 @app.context_processor
 def navbar_context():
     pages = [table for table in Tables.keys()]
-    return {"pages": pages}
+    return {'pages': pages}
 
 
-@app.route("/")
+@app.route('/')
 def Index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 
 @app.route('/tari')
 def tari():
-    tableName = "tari"
+    tableName = 'tari'
+    session["tableName"] = tableName
     table = Tables[tableName]
     columnNames = table.__table__.columns.keys()
     tableData = db.session.query(table).all()
-    return render_template(f"{tableName}.html", tableName=tableName.title(), columnNames=columnNames, tableData=tableData)
+    return render_template('masterfile.html', tableName=tableName, columnNames=columnNames, tableData=tableData)
 
 
 @ app.route('/echipe')
 def echipe():
-    tableName = "echipe"
+    tableName = 'echipe'
+    session["tableName"] = tableName
     table = Tables[tableName]
     columnNames = table.__table__.columns.keys()
     tableData = db.session.query(table).all()
-    return render_template(f"{tableName}.html", tableName=tableName.title(), columnNames=columnNames, tableData=tableData)
+    return render_template('masterfile.html', tableName=tableName, columnNames=columnNames, tableData=tableData)
 
 
 @ app.route('/utilizatori')
 def utilizatori():
-    tableName = "utilizatori"
+    tableName = 'utilizatori'
+    session["tableName"] = tableName
     table = Tables[tableName]
     columnNames = table.__table__.columns.keys()
     tableData = db.session.query(table).all()
-    return render_template(f"{tableName}.html", tableName=tableName.title(), columnNames=columnNames, tableData=tableData)
+    return render_template('masterfile.html', tableName=tableName, columnNames=columnNames, tableData=tableData)
 
 
 @ app.route('/autori')
 def autori():
-    tableName = "autori"
+    tableName = 'autori'
+    session["tableName"] = tableName
     table = Tables[tableName]
     columnNames = table.__table__.columns.keys()
     tableData = db.session.query(table).all()
-    return render_template(f"{tableName}.html", tableName=tableName.title(), columnNames=columnNames, tableData=tableData)
+    return render_template('masterfile.html', tableName=tableName, columnNames=columnNames, tableData=tableData)
 
 
 @ app.route('/concursuri_ctf')
 def concursuri_ctf():
-    tableName = "concursuri_ctf"
+    tableName = 'concursuri_ctf'
+    session["tableName"] = tableName
     table = Tables[tableName]
     columnNames = table.__table__.columns.keys()
     tableData = db.session.query(table).all()
-    return render_template(f"{tableName}.html", tableName=tableName.title(), columnNames=columnNames, tableData=tableData)
+    return render_template('masterfile.html', tableName=tableName, columnNames=columnNames, tableData=tableData)
 
 
 @ app.route('/probleme')
 def probleme():
-    tableName = "probleme"
+    tableName = 'probleme'
+    session["tableName"] = tableName
     table = Tables[tableName]
     columnNames = table.__table__.columns.keys()
     tableData = db.session.query(table).all()
-    return render_template(f"{tableName}.html", tableName=tableName.title(), columnNames=columnNames, tableData=tableData)
+    return render_template('masterfile.html', tableName=tableName, columnNames=columnNames, tableData=tableData)
 
 
 @ app.route('/rezolvari')
 def rezolvari():
-    tableName = "rezolvari"
+    tableName = 'rezolvari'
+    session["tableName"] = tableName
     table = Tables[tableName]
     columnNames = table.__table__.columns.keys()
     tableData = db.session.query(table).all()
-    return render_template(f"{tableName}.html", tableName=tableName.title(), columnNames=columnNames, tableData=tableData)
+    return render_template('masterfile.html', tableName=tableName, columnNames=columnNames, tableData=tableData)
 
 
 @ app.route('/servere')
 def servere():
-    tableName = "servere"
+    tableName = 'servere'
+    session["tableName"] = tableName
     table = Tables[tableName]
     columnNames = table.__table__.columns.keys()
     tableData = db.session.query(table).all()
-    return render_template(f"{tableName}.html", tableName=tableName.title(), columnNames=columnNames, tableData=tableData)
+    return render_template('masterfile.html', tableName=tableName, columnNames=columnNames, tableData=tableData)
 
 
 @ app.route('/incercari')
 def incercari():
-    tableName = "incercari"
+    tableName = 'incercari'
+    session["tableName"] = tableName
     table = Tables[tableName]
     columnNames = table.__table__.columns.keys()
     tableData = db.session.query(table).all()
-    return render_template(f"{tableName}.html", tableName=tableName.title(), columnNames=columnNames, tableData=tableData)
+    return render_template('masterfile.html', tableName=tableName, columnNames=columnNames, tableData=tableData)
+
+
+@app.route('/add', methods=['POST'])
+def add():
+    if request.method == 'POST':
+        if "tableName" in session:
+            try:
+                tableName = session.get("tableName")
+                table = Tables[tableName]
+
+                entry = table(**request.form)
+
+                db.session.add(entry)
+                db.session.commit()
+                flash("Adaugat cu succes!")
+            except Exception as e:
+                flash(str(e))
+            finally:
+                return redirect(url_for(f'{tableName}'))
+        else:
+            return redirect(url_for("index"))
+
+
+@app.route('/update')
+def update():
+    return
+
+
+@app.route('/delete')
+def delete():
+    return
